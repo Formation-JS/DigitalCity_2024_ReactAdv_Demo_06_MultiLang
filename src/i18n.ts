9d1d0d89fcbf3a79004dcdc3bdcfb05f4689a158
Aurélien St <aurelien.strimelle@bstorm.be>
2024-04-10 16:48:13 +0200

    Move lang file into public folder

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,26 +1,17 @@
 import i18n from 'i18next';
+import i18NextHttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next)
+i18n.use(i18NextHttpBackend)
+    .use(initReactI18next)
     .init({
         lng: 'en',
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false // React le fait déjà
         },
-        resources: {
-            en: {
-                translation: {
-                    title: 'Hello World',
-                    desc: 'This is a demo'
-                }
-            },
-            fr: {
-                translation: {
-                    title: 'Bonjour le monde',
-                    desc: 'Ceci est un démo'
-                }
-            }
+        backend: {
+            loadPath: 'http://localhost:5173/lang/{{lng}}.json'
         }
     });
 
